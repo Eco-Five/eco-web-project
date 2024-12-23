2ceba75c8d7234def54d94ba45b749bcef4e38a6
hoon0711 <doh40711@gmail.com>
2024-12-23 18:21:06 +0900

    google login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'main', pageName: 'home.ejs' });
+  res.render('index', { title: 'main', pageName: 'main/home.ejs' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: '로그인', pageName: 'login/login.ejs' });
+});
+
+router.get('/login/find', function(req, res, next) {
+  res.render('index', { title: '회원 정보 찾기', pageName: 'login/find.ejs' });
 });
 
 module.exports = router;
