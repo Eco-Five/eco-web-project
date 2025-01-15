9038eebf58baeacfc147d2b16313f18bab52171f
hogi2020 <junmyung2020@gmail.com>
2025-01-16 01:34:12 +0900

    build main page

--- routes/api.js
+++ routes/api.js
@@ -247,7 +247,7 @@ router.post('/memberLogin', async (req, res) => {
             req.session.user = await sessionInfo(userInfo)
             res.status(200).json({ message: '로그인 성공', result: match })
         } else {
-            res.status(401).json({ message: '계정이 일치하지 않습니다.', result: match })
+            res.status(200).json({ message: '계정이 일치하지 않습니다.', result: match })
         }
     } catch (error) {
         console.error("memberLogin 오류: ", error);
@@ -384,9 +384,9 @@ router.get('/signup/redirect', async (req, res) => {
 /************************************** Session Mng **************************************/
 router.get('/protected', (req, res) => {
     if (req.session?.user?.isAuthenticated) {
-        res.status(200).json({ message: '인증된 사용자 입니다.', user: req.session.user });
+        res.status(200).json({ message: '인증된 사용자 입니다.', auth: true });
     } else {
-        res.status(401).json({ message: '로그인이 필요합니다.' }); // 인증되지 않은 경우 401 상태 코드 반환
+        res.status(401).json({ message: '로그인이 필요합니다.', auth: false });
     }
 })
 
